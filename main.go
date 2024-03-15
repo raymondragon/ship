5075802c82d86bd0644ea12747fee38424d8f8f5
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-15 09:58:39 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "flag"
+    "log"
+
+    "github.com/raymondragon/golib"
+)
+
+var rawURL = flag.String("url", "", "http(s)://user:pass@host:port")
+
+func main() {
+    flag.Parse()
+    parsedURL, err := golib.URLParse(*rawURL)
+    if err != nil {
+        log.Printf("[WARN] %v", err)
+    }
+    proxyHandler := golib.ProxyHandler(parsedURL.Hostname, parsedURL.Username, parsedURL.Password, nil)
+    switch parsedURL.Scheme {
+    case "http":
+        log.Printf("[INFO] %v", *rawURL)
+        if err := golib.ServeHTTP(parsedURL.Hostname, parsedURL.Port, proxyHandler); err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+    case "https":
+        tlsConfig, err := golib.TLSConfigApplication(parsedURL.Hostname)
+        if err != nil {
+            log.Printf("[WARN] %v", err)
+            tlsConfig, err = golib.TLSConfigGeneration(parsedURL.Hostname)
+            if err != nil {
+                log.Printf("[WARN] %v", err)
+            }
+        }
+        log.Printf("[INFO] %v", *rawURL)
+        if err := golib.ServeHTTPS(parsedURL.Hostname, parsedURL.Port, proxyHandler, tlsConfig); err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+    default:
+        log.Fatalf("[ERRO] %v", parsedURL.Scheme)
+    }
+}
